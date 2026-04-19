b1a4fd627924fa02367eeaeaaa153d9fbfa9b5e8
sudoer_wsl_r9950x <wsl_r9950x@sudoer.cn>
2026-04-20 01:26:35 +0800

    删除阿里resp接口后端

--- __init__.py
+++ __init__.py
@@ -1,7 +1,6 @@
 from .CoTChatAgent import CoTChatAgent
 from .VisionChatAgent import VisionChatAgent
 from .AgentBase import Agent_OpenAIChat_API_Backend
-from .AliBailian_RESP import AliBailian_RESP_Agent
 from .Skill import Skill
 from .ToolBase import ToolBase
 from .ToolNormal import ToolNormal
@@ -11,7 +10,6 @@ __all__ = [
     "CoTChatAgent",
     "VisionChatAgent",
     "Agent_OpenAIChat_API_Backend",
-    "AliBailian_RESP_Agent",
     "Skill",
     "ToolBase",
     "ToolNormal",
