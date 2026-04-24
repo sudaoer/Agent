7e61cbd2a70d106418e2189225923b58681de95e
sudoer_wsl_r9950x <wsl_r9950x@sudoer.cn>
2026-04-24 23:52:23 +0800

    根据dsv4的要求修改

--- CoTChatAgent.py
+++ CoTChatAgent.py
@@ -1,5 +1,4 @@
 # 对于有思维链且可以在思维链中调用工具的模型，在tool call上需要特殊处理
-from copy import deepcopy
 from typing import Any, Optional
 
 from .AgentBase import Agent_OpenAIChat_API_Backend
@@ -22,13 +21,5 @@ class CoTChatAgent(Agent_OpenAIChat_API_Backend):
         extra_body: Optional[dict[str, Any]] = None,
     ) -> tuple[list[dict[str, Any]], dict[str, Any]]:
         prepared_extra_body = dict(extra_body) if extra_body is not None else {}
-        prepared_extra_body["enable_thinking"] = True
-        history_tmp = deepcopy(history)
-        start_del = False
-        # 删除最后一条真实用户消息之前所有的reasoning_content
-        for i in range(len(history) - 1, -1, -1):
-            if history_tmp[i]["role"] == "user":
-                start_del = True
-            if start_del and "reasoning_content" in history_tmp[i]:
-                del history_tmp[i]["reasoning_content"]
-        return history_tmp, prepared_extra_body
+        prepared_extra_body["thinking"] = {"type": "enabled"}
+        return history, prepared_extra_body
